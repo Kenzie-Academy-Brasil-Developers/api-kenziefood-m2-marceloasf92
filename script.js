248f1ff5c9c6cf504edb65b0c72a320587df8d70
Marcelo Alves <marcelo.asf92@gmail.com>
2022-01-25 10:14:09 -0300

    enviando primeira v.00

---
+++ script.js
@@ -0,0 +1,8 @@
+import { VitrineModel } from "./src/models/Vitrine.js";
+import { VitrineController } from "./src/controllers/Vitrine.js"
+import { db } from "./src/mock/db.js";
+
+
+VitrineController.criarTemplate(db)
+
+
